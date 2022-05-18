dbbfc50b883d4b0e599aeb55a3b6c62c53eb9de3
nat corpuz <nate.corpuz1@gmail.com>
2022-05-18 12:00:03 +0800

    wip environment

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 /* Dependencies / Modules */
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require('dotenv'); //reads .env file
+
 
 
 /* Environment Setup */
+dotenv.config(); //setup environment
+let dbConn = process.env.DB_CONNECTION;
+let tester = process.env.ANOTHER_VAL;
+console.log(tester);
 const port = 4000;
+
 
 
 /* Server Setup */
 const app = express();
+
 
 
 /* Middlewares */
@@ -16,16 +24,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-/* Database Connection */
-mongoose.connect(
-	"mongodb+srv://natcorpuz:admin123@cluster0.yso0q.mongodb.net/?retryWrites=true&w=majority"
-);
 
+/* Database Connection */
+mongoose.connect(dbConn);
 let connectStatus = mongoose.connection;
 connectStatus.on("error", console.error.bind(console, "connection error"));
 connectStatus.once("open", () => console.log("--> Connected to Database"));
 
+
+
 /* Backend Routes */
+
+
+
 
 
 /* Server Gateway Response */
