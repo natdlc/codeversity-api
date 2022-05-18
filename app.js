9555a368edaa0a428e0adccbb2dc01dd37473af1
nat corpuz <nate.corpuz1@gmail.com>
2022-05-18 13:40:26 +0800

    integrate environment setup

--- app.js
+++ app.js
@@ -4,19 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv'); //reads .env file
 
 
-
 /* Environment Setup */
 dotenv.config(); //setup environment
-let dbConn = process.env.DB_CONNECTION;
-let tester = process.env.ANOTHER_VAL;
-console.log(tester);
-const port = 4000;
-
+let acct = process.env.CRED;
+const port = process.env.PORT;
 
 
 /* Server Setup */
 const app = express();
-
 
 
 /* Middlewares */
@@ -24,18 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-
 /* Database Connection */
-mongoose.connect(dbConn);
+mongoose.connect(acct);
 let connectStatus = mongoose.connection;
-connectStatus.on("error", console.error.bind(console, "connection error"));
 connectStatus.once("open", () => console.log("--> Connected to Database"));
 
 
-
 /* Backend Routes */
-
-
 
 
 
